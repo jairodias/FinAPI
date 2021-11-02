686e71da75bec1cf474b4e59030abb3e1ec95d4b
jairodias <jairopereira_dias@hotmail.com>
2021-11-02 17:17:42 -0300

    garantindo a integridade das contas e rota para retorno dos extratos bancarios

--- src/index.js
+++ src/index.js
@@ -1,5 +1,45 @@
 const express = require('express');
+const { v4: uuid } = require("uuid");
 
 const app = express();
 
-app.listen(3333, () => console.log('cheguei'));
+app.use(express.json())
+
+const customers = [];
+
+app.post("/account", (request, response) => {
+    const { document, name } = request.body;
+
+    const customerAlreadyExists = customers.some((customer) => customer.document === document);
+
+    if (customerAlreadyExists) {
+        return response.status(400).json({
+            error: "Customer alerady exists."
+        });
+    }
+
+    customers.push({
+        document,
+        name,
+        id: uuid(),
+        statement: []
+    });
+
+    return response.status(201).send();
+});
+
+app.get("/statement/:document", (request, response) => {
+    const { document } = request.params;
+
+    const customer = customers.find((customer) => customer.document === document);
+
+    if (!customer) {
+        return response.status(400).json({
+            error: "Customer not found."
+        });
+    }
+
+    return response.json(customer.statement);
+});
+
+app.listen(3333);
